ae6b5b263f0bf1feace9ee9477ec3b8ba5a5ff11
AlejandroMontalvo <alejandrormont@gmail.com>
2022-06-08 15:55:00 -0400

    Update listings.php

--- database_interactions/marketplace/listings.php
+++ database_interactions/marketplace/listings.php
@@ -4,7 +4,7 @@
         for ($i=0; $i<$listings_amount; $i+=1) {
             echo("<a class='listing' href='index.php'>");
             echo("<img src='./imgs/apple.jpg' alt='Logout'/>");
-            echo("<div class='listingTitleSpace'><p class='listingTitle'>asdasd.asdaasdasd.</p></div>");
+            echo("<div class='listingTitleSpace'><p class='listingTitle'>Fresh Apple</p></div>");
             echo("<p class='listingPrice'><strong>$5.00</strong></p>");
             echo("</a>");
         }
